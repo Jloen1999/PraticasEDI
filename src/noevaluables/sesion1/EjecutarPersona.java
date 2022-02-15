7ef1afe4a8ffa02afe5017c23ade353e32ffde53
jloen1999 <jloelananguan@gmail.com>
2022-02-15 11:35:47 +0100

    Introducimos los datos por teclado

--- src/noevaluables/sesion1/EjecutarPersona.java
+++ src/noevaluables/sesion1/EjecutarPersona.java
@@ -1,9 +1,19 @@
 package noevaluables.sesion1;
+import java.lang.String;
+import java.awt.image.ImagingOpException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class EjecutarPersona {
-    public static void main(String[] args){
+
+    public static void main(String[] args) throws IOException {
+         InputStreamReader read=new InputStreamReader(System.in);
+         BufferedReader input=new BufferedReader(read);
         Persona per=new Persona("Jose Luis","Ela Nanguan",21,232324);
+        Persona per1=new Persona();
         System.out.println(per.toString());
+        System.out.println("Nombre: ",per.setNombre(input.readLine()));
     }
 
 }
