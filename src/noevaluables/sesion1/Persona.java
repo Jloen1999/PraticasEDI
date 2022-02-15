190bd6848ec8948bbb4fa8182b87ec2c0210230a
jloen1999 <jloelananguan@gmail.com>
2022-02-15 01:31:28 +0100

    Metodos setters

--- src/noevaluables/sesion1/Persona.java
+++ src/noevaluables/sesion1/Persona.java
@@ -30,4 +30,20 @@ public class Persona {
     public int getDni() {
         return dni;
     }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
 }
