fcfe3b6ffd9bb7eab3281ee2ec612cf3dfff3d87
jloen1999 <jloelananguan@gmail.com>
2022-02-15 11:28:04 +0100

    Instanciacion y uso del toString()

---
+++ src/noevaluables/sesion1/EjecutarPersona.java
@@ -0,0 +1,9 @@
+package noevaluables.sesion1;
+
+public class EjecutarPersona {
+    public static void main(String[] args){
+        Persona per=new Persona("Jose Luis","Ela Nanguan",21,232324);
+        System.out.println(per.toString());
+    }
+
+}
