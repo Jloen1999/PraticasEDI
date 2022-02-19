f6c3462a572c3be6d25d43909dcb387b70490dc6
jloen1999 <jloelananguan@gmail.com>
2022-02-19 14:10:37 +0100

    Conversion Grados Celsius a Farenheit

--- src/noevaluables/sesion0/ConversionGradosCelsiusFarenheit.java
+++ src/noevaluables/sesion0/ConversionGradosCelsiusFarenheit.java
@@ -1,35 +1,33 @@
 package noevaluables.sesion0;
+import java.lang.System;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-
-
-public class ConversionGradosCelsiusFarenheit {
-    public ConversionGradosCelsiusFarenheit()throws IOException
-    {
+public class ConversionGradosCelsiusFarenheit{
+    public ConversionGradosCelsiusFarenheit() throws IOException{
         InputStreamReader read=new InputStreamReader(System.in);
         BufferedReader input=new BufferedReader(read);
-        double C, F;
+        double C,F;
         System.out.print("Introduce los grados Celsius: ");
         C=Double.parseDouble(input.readLine());
         System.out.print("Introduce los grados Farenheit: ");
         F=Double.parseDouble(input.readLine());
+
+        System.out.println("Los grados Farenheit son: "+F);
         System.out.println("Los grados Celsius son: "+C);
-        System.out.print("Los grados Farenheit son: "+F);
 
     }
 
-    public double convertirACelsius(double farenheit){
-        return (farenheit-32)/1.8;
+    public double convertirACelsius(double F){
+        return (F-32)/1.8;
     }
-
-    public double convertirAFarenheit(double Celsius){
-        return 1.8*Celsius+32;
-    }
-    public static void main(String[] args) throws IOException
-    {
-          new ConversionGradosCelsiusFarenheit();
+    public double convertirAFarenheit(double C){
+        return 1.8*C+32;
     }
 
 
+
+    public static void main(String[] args) throws IOException{
+        new ConversionGradosCelsiusFarenheit();
+    }
 }
