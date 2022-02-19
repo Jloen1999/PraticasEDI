fee70c9b2cc23ee127785ce7b43db47c9546c657
jloen1999 <jloelananguan@gmail.com>
2022-02-19 18:45:46 +0100

    Metodos hashCode y equals

--- src/noevaluables/sesion1/Persona.java
+++ src/noevaluables/sesion1/Persona.java
@@ -10,6 +10,8 @@
 //Primero llamamos a las bibliotecas necesarias
 package noevaluables.sesion1;
 
+import java.util.Objects;
+
 public class Persona { //Creamos la clase con el mismo nombre que el fichero
     //Creamos los atributos
     private String nombre;
@@ -17,12 +19,15 @@ public class Persona { //Creamos la clase con el mismo nombre que el fichero
     private int edad;
     private int dni;
     private char sexo;
+    private int numPersonas=0;
 
     public Persona() { //Creamos el constructor por defecto
         nombre = "Jose Luis";
         apellidos = "Ela Nanguan";
         edad = 21;
         dni = 123456;
+        sexo='H';
+        contarPersona();
     }
 
     public Persona(String nombre, String apellidos, int edad, int dni, char sexo) {//Creamos el constructor parametrizado
@@ -30,66 +35,109 @@ public class Persona { //Creamos la clase con el mismo nombre que el fichero
         this.apellidos = apellidos;
         this.edad = edad;
         this.dni = dni;
+        this.sexo=sexo;
+        contarPersona();
     }
+
+    public Persona(Persona p){
+        this.nombre=p.getNombre();
+        this.apellidos=p.getApellidos();
+        this.dni=p.getDni();
+        this.edad=p.getEdad();
+    }
+
+    public Persona mostrarPersonaClonada(){
+        return new Persona(this.nombre,this.apellidos,this.edad,this.dni,this.sexo);
+    }
+
+    public char getSex()
+    {
+        return sexo;
+    }
+
+    public void contarPersona(){
+        numPersonas++;
+    }
+    public int mostrarTotalPersonas(){
+        return numPersonas;
+    }
+
+
 
     //Luego creamos los metodos getter and setter
-    public void getSexo(char sexo){
-        this.sexo=sexo;
-    }
-
-    public String setSexo(){
-        String s;
+    public String getSexo(){
+        String s = "";
         sexo=Character.toUpperCase(sexo); //Convertimos el sexo introducido en mayuscula de manera que no me importe si el usuairo a introducido el sexo en mayuscula o en minuscula
         s = switch (sexo) {
-            case 'h' -> "Hombre";
-            case 'm' -> "mujer";
+            case 'H' -> "Hombre";
+            case 'M' -> "Mujer";
             default -> "Desconocido";
         };
 
         return s;
     }
+    public void setSexo(char s) {
+        sexo=s;
+    }
 
-    public void getNombre(String nombre) {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public String setnombre() {
+    public String getNombre() {
         return nombre;
 
     }
 
-    public void getApellidos() {
+    public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
-    public String setApellidos(String apellidos) {
+    public String getApellidos() {
         return apellidos;
     }
 
-    public void getDni() {
+    public void setDni(int dni) {
         this.dni = dni;
     }
 
-    public int setDni(int dni) {
+    public int getDni() {
         return dni;
     }
 
-    public void getEdad() {
+    public void setEdad(int edad) {
         this.edad = edad;
     }
 
-    public int setEdad(int edad) {
+    public int getEdad() {
         return edad;
     }
 
     @Override
-    public String toString() {//Creamos el metodo toString para mostrar los valores de los atributos de la clase
-        return "Persona{" +
-                "nombre='" + nombre + '\'' +
-                ", apellidos='" + apellidos + '\'' +
-                ", edad=" + edad +
-                ", dni=" + dni +
-                ", sexo=" + sexo +
-                '}';
+    public String toString(){
+      return "Persona{" +
+              "Nombre='"+getNombre()+'\''+
+              ", apellidos='"+getApellidos()+'\''+
+              ", edad="+getEdad()+
+              ", dni="+getDni()+
+
+              "}";
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        boolean result;
+        if(this==o) result=true;
+        if(o==null || getClass() != o.getClass()) result=false;
+        Persona p= (Persona) o;
+        result= (dni==p.getDni() && edad==p.getEdad() && Objects.equals(nombre,p.getNombre()) && Objects.equals(apellidos,p.getApellidos()));
+        return result;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, apellidos, edad, dni, sexo, numPersonas);
     }
 }
