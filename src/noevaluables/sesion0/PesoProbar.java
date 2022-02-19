93dc12c5f9bf4390ebb515ae981d6497acbce0ad
jloen1999 <jloelananguan@gmail.com>
2022-02-19 16:38:28 +0100

    Probar Peso

---
+++ src/noevaluables/sesion0/PesoProbar.java
@@ -0,0 +1,11 @@
+package noevaluables.sesion0;
+
+public class PesoProbar {
+    public static void main(String[] args)
+    {
+        Peso p=new Peso(23,"lb");
+        System.out.println(p.toString());
+        System.out.println("Gramos: "+p.getGramos());
+
+    }
+}
