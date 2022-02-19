bfc0363dd468424cc14ec5e7bfec0b025da7424f
jloen1999 <jloelananguan@gmail.com>
2022-02-19 15:18:09 +0100

    Clase MiNumero

---
+++ src/noevaluables/sesion0/MiNumero.java
@@ -0,0 +1,42 @@
+package noevaluables.sesion0;
+
+public class MiNumero {
+    private int numero;
+    public MiNumero(){
+        numero=0;
+    }
+    public MiNumero(int num){
+        this.numero=num;
+    }
+    public void setNumero(int num){
+        this.numero=num;
+    }
+    public int getNumero(){
+        return numero;
+    }
+
+    public void suma(int num2){
+        System.out.println(numero+num2);
+    }
+    public void resta(int num2){
+        System.out.println(numero-num2);
+    }
+    public int getDouble(){
+        return numero*2;
+    }
+    public int getTriple(){
+        return numero*3;
+    }
+    public int getCuadruple(){
+        return numero*4;
+    }
+
+    @Override
+    public String toString(){
+       return "Numeros{" +
+               " Doble: " +getDouble()+
+               " Triple: " +getTriple()+
+               " Cuadruple: "+getCuadruple()+
+               "}" ;
+    }
+}
