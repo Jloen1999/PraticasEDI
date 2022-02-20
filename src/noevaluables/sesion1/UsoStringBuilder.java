5eb3cf614e556ecb77d26e8c0d2940ba64e0838f
jloen1999 <jloelananguan@gmail.com>
2022-02-20 21:52:14 +0100

    Uso de la clase StringBuilder

---
+++ src/noevaluables/sesion1/UsoStringBuilder.java
@@ -0,0 +1,11 @@
+package noevaluables.sesion1;
+import java.lang.StringBuilder;
+public class UsoStringBuilder {
+  public static void main(String[] args)
+  {
+      StringBuilder cadena=new StringBuilder("Jose Luis");
+      cadena.append(" Ela Nanguan");
+      cadena.delete(1,4);
+      System.out.println(cadena.toString());
+  }
+}
