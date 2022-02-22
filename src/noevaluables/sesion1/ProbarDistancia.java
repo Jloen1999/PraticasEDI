5fbf9e858cd4ba8724000f355df69a8fb93c95aa
jloen1999 <jloelananguan@gmail.com>
2022-02-22 12:33:16 +0100

    Clase probar Distancia

---
+++ src/noevaluables/sesion1/ProbarDistancia.java
@@ -0,0 +1,13 @@
+package noevaluables.sesion1;
+import java.util.Scanner;
+public class ProbarDistancia {
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        Distancia dist=new Distancia();
+        System.out.print("Introduce la cantidad de millas: ");
+        dist.setMillas(input.nextInt());
+        System.out.println(dist.toString());
+
+    }
+}
