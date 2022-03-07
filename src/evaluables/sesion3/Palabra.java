6d480d3cec9d30c1e316d4b2dc9be4b71f1cad70
jloen1999 <jloelananguan@gmail.com>
2022-03-07 11:25:54 +0100

    Constructor por defecto

--- src/evaluables/sesion3/Palabra.java
+++ src/evaluables/sesion3/Palabra.java
@@ -2,5 +2,11 @@ public class Palabra{
     protected String texto;
     protected String pista;
     protected int longitud;
+
+    public Palabra(){
+        texto=new String();
+        pista=new String();
+        longitud=texto.length();
+    }
     
 }
