1c7e446247fe3b8e71d0f93f2b36ee5599afcfbb
DEVIT <74373827+Jloen1999@users.noreply.github.com>
2022-03-11 20:50:22 +0100

    Update Ahorcado.java

--- src/evaluables/sesion5/Ahorcado.java
+++ src/evaluables/sesion5/Ahorcado.java
@@ -11,19 +11,11 @@ public class Ahorcado {
     private Palabra seleccionado;
     private Palabra[] lista;
 
-    private int numPalabrasQueTengo;
-    private int numLetrasAcertadas;
-    private boolean[] aciertos;
-
-
     public Ahorcado() {
         intentoActual = 0;
         numIntentos = 5;
         seleccionado = new Palabra();
         lista = new Palabra[4];
-        numPalabrasQueTengo = 0;
-        numLetrasAcertadas = 0;
-        aciertos = new boolean[seleccionado.getTexto().length()];
     }
 
     public Ahorcado(int tam, int max) {
@@ -84,60 +76,5 @@ public class Ahorcado {
         return add;
     }
 
-    //Introducimos la lógica del juego
-    public int controlarPos(int pos) throws IOException {//Controlamos que la posición introducida esté entre 0 y la cantidad de palabras que tengo
-        t = new Teclado();//Instanciamos un objeto de la clase teclado
-        while (pos < 0 || pos > numPalabrasQueTengo) {
-            pos = t.literalConEntero("Introduce una nueva posicion: ");//El usuario tiene que volver a introducir la posición cuando siempre que no se cumpla la condicion
-        }
-        return pos;//Devolvemos la posicion correcta
-    }
-
-    public void seleccionarPalabra(int pos) throws IOException {//Método para seleccionar la palabra desde una posición especificada
-        pos = controlarPos(pos);
-        seleccionado = lista[pos];//Almacenamos la palabra que ocupa la posición especificada
-        aciertos = new boolean[seleccionado.getTexto().length()];//Asignamos el array de aciertos un tamaño que sea igual al tamaño de la palabra seleccionada
-
-    }
-
-    public void seleccionarPalabra() {//Metodo que selecciona una palabra en una posicion aleatoria del array
-        Random r = new Random(); //Primero creamos un objeto de la clase Random
-        int alea = r.nextInt(numPalabrasQueTengo);//Almacenamos un numero aleatorio entre el 0(incluido) y numPalabrasQueTengo(excluido) en una variable de tipo entero
-        seleccionado = lista[alea];//Obtenemos la palabra seleccionada que ocupa dicha posicion aleatoria
-        aciertos = new boolean[seleccionado.getTexto().length()];//Asignamos el array de aciertos un tamaño que sea igual al tamaño de la palabra seleccionada
-    }
-
-    public boolean descubrirLetra(char letra) {//Metodo: Dada una letra comprobamos que se encuentra en la palabra seleccionada
-        boolean enc = false;//Booleano para comprobar si la letra es encontrada
-        String texto = seleccionado.getTexto();//Obtenemos la palabra seleccionada;
-        //Recorremos el array de todos los caracteres de la palabra seleccionada
-        for (int i = 0; i < texto.length(); i++) {
-            if (letra == texto.charAt(i) && !aciertos[i]) {//Si la letra no esta y he acertado
-                enc = true;//si la letra es encontrada
-                numLetrasAcertadas++;//incrementamos el numero de letras acertadas
-                aciertos[i] = true;//Almacenamos en un array de booleanos
-            }
-        }
-
-        if (enc) {
-            return true;
-        } else {
-            intentoActual++;
-            return false;
-        }
-
-    }
-
-    public boolean siGanado() { //Si ya esta todo acertado
-        if(numLetrasAcertadas==seleccionado.getTexto().length()){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean siFracaso(){
-        return intentoActual==numIntentos;
-    }
 
 }
